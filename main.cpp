0e3d633d4b4006666397e352a2c2a22cd1912d27
Parth Songire <111558783+parth-songire23@users.noreply.github.com>
2022-08-21 17:32:00 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -56,6 +56,48 @@ void InsertionSort(int A[], int n)
     }
 }
 
+void MergeSort(int A[],int l, int mid, int h)
+{
+    int i,j,k;
+    int B[h++];
+    i=l;j=mid+1;k=l;
+    
+    while(i<=mid && j<=h)
+    {
+        if(A[i]<A[j])
+            B[k++]=A[i++];
+        else
+            B[k++]=A[j++];
+    }
+    
+    for(;i<=mid;i++)
+        B[k++]=A[i];
+    for(;j<=h;j++)
+        B[k++]=A[j];
+    
+    for(i=l;i<=h;i++)
+        A[i]=B[i];
+}
+
+
+void IMergeSort(int A[],int n)
+{
+    int p,i,mid,l,h=0;
+    
+    for(p=2;p<=n;p=p*2)
+    {
+        for(i=0;i+p-1<n;i=i+p)
+        {
+            l=i;
+            h=i+p-1;
+            mid=(l+h)/2;
+            MergeSort(A,l,mid,h);
+        }
+    }
+    if(p/2<n)
+        MergeSort(A,0,p/2,n-1);
+}
+
 int main()
 {
 //    your code goes here
