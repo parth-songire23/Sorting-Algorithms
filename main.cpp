ab8dea2756e2ffbe2aeb2a53b094f4096ad27017
Parth Songire <111558783+parth-songire23@users.noreply.github.com>
2022-09-13 12:07:01 +0530

    Quick Sort added

--- main.cpp
+++ main.cpp
@@ -122,6 +122,34 @@ void InsertionSort(int A[], int n)
     }
 }
 
+int partition(int A[],int l,int h)
+{
+    int pivot = A[l];
+    int i=l,j=h;
+    do
+    {
+        do{i++;}while(A[i]<=pivot);
+        do{j--;}while(A[j]>pivot);
+    
+        if(i<j)swap(&A[i],&A[j]);
+    }while(i<j);
+    
+    swap(&A[l],&A[j]);
+    return j;
+}
+
+void Quicksort(int A[],int l,int h)
+{
+    int j;
+    if(l<h)
+    {
+        j=partition(A, l, h);
+        Quicksort(A, l, j);
+        Quicksort(A, j+1, h);
+    }
+}
+
+
 int main()
 {
 //    your code goes here
